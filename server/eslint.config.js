5538bdc677b6e14aca95b31c7d55c60a8a5913c1
B. Rozmarin <batshevar@skyvar.co.il>
2025-12-30 14:42:21 +0200

    ☝️ Try again

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,29 +1,29 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  env: {
-    node: true,
-    jest: true,
-    es2020: true
-  },
-  globals: {
-    process: 'readonly',
-    console: 'readonly'
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn'
-  },
-  overrides: [
-    {
-      files: ['**/*.ts'],
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    env: {
+      node: true,
+      jest: true,
+      es2020: true
+    },
+    globals: {
+      process: "readonly",
+      console: "readonly",
+      jest: "readonly",
+      describe: "readonly",
+      it: "readonly",
+      expect: "readonly"
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn"
     }
-  ]
-};
+  }
+];
