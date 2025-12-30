468586a76fd3e2aefe2d5a3c6398471a44575a80
B. Rozmarin <batshevar@skyvar.co.il>
2025-12-30 14:11:23 +0200

    ☝️ Add lint to package.json

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -1,18 +1,6 @@
-export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2020,
-      sourceType: "module",
-    },
-    env: {
-      node: true,   
-      jest: true,   
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
-  },
-];
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts']
+};
